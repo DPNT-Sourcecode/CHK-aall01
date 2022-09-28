63cfce619a2f6a9239417a7d4bea7a5c738ec944
sbx_user1051 <sbx_user1051@169.254.12.189>
2022-09-28 10:38:42 +0000

    Wed Sep 28 10:38:42 UTC 2022

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -6,7 +6,33 @@ public class CheckoutSolution {
     public Integer checkout(String skus) {
         if(checkString(skus)!=true)
             return -1;
+        ShoppingCart shoppingCart = new ShoppingCart(skus);
 
+        // first we should handle the get One Free Offer
+        // as those interact with other items as well
+        shoppingCart.getOneFreeOffer(letterToCode('E'),2,letterToCode('B'));
+        shoppingCart.getOneFreeOffer(letterToCode('N'),3,letterToCode('M'));
+        shoppingCart.getOneFreeOffer(letterToCode('R'),3,letterToCode('Q'));
+
+        // for F and U we need a special method which should handle the price as well
+        // because the same item is affected
+        shoppingCart.getOneFreeOfferSameItem(letterToCode('F'),2,10);
+        shoppingCart.getOneFreeOfferSameItem(letterToCode('U'),3,40);
+
+        // now handling the get X for lower price offers
+        shoppingCart.getXForLowerPriceOffer(letterToCode('A'),200,5);
+        shoppingCart.getXForLowerPriceOffer(letterToCode('A'),130,3);
+        shoppingCart.getXForLowerPriceOffer(letterToCode('B'),45,2);
+        shoppingCart.getXForLowerPriceOffer(letterToCode('H'),80,10);
+        shoppingCart.getXForLowerPriceOffer(letterToCode('P'),200,5);
+        shoppingCart.getXForLowerPriceOffer(letterToCode('Q'),80,3);
+        shoppingCart.getXForLowerPriceOffer(letterToCode('V'),130,3);
+        shoppingCart.getXForLowerPriceOffer(letterToCode('V'),90,2);
+
+        // processing the rest of the shopping cart
+
+
+        return shoppingCart.getTotal();
     }
 
     //function for checking if the input string is valid.
@@ -17,4 +43,10 @@ public class CheckoutSolution {
         }
         return true;
     }
+    // converts from letter to its code in the numbering system
+    // used by the system. Example: A - 0, B - 1,...
+    public static int letterToCode(char letter){
+        return (int)letter - 65;
+    }
 }
+
--- src/main/java/befaster/solutions/CHK/ShoppingCart.java
+++ src/main/java/befaster/solutions/CHK/ShoppingCart.java
@@ -1,8 +1,8 @@
 package befaster.solutions.CHK;
 
 public class ShoppingCart {
-    int[] itemsArray;
-    int total=0;
+    private int[] itemsArray;
+    private int total=0;
 
     public ShoppingCart(String skus){
         itemsArray = new int[26];
@@ -24,6 +24,15 @@ public class ShoppingCart {
             if(itemsArray[discountedItem]<0)itemsArray[discountedItem]=0;
         }
     }
+    public void getOneFreeOfferSameItem(int itemId, int numberOfItemsForDiscount,int price){
+        if (itemsArray[itemId]>=numberOfItemsForDiscount){
+            int pairs_of_prod=itemsArray[itemId] /numberOfItemsForDiscount;
+            total += numberOfItemsForDiscount * price *pairs_of_prod;
+            itemsArray[itemId] -= pairs_of_prod* numberOfItemsForDiscount;
+            itemsArray[itemId] -= pairs_of_prod;
+            if (itemsArray[itemId]<0) itemsArray[itemId]=0;
+        }
+    }
     public void getXForLowerPriceOffer(int itemId, int offerPrice, int noItemsToGetDiscount){
         if (itemsArray[itemId]>=noItemsToGetDiscount) {
             total += (itemsArray[itemId]/noItemsToGetDiscount)*offerPrice;
@@ -31,4 +40,8 @@ public class ShoppingCart {
             itemsArray[itemId] = itemsArray[offerPrice] - removedProducts*noItemsToGetDiscount;
         }
     }
+    public void buyNoOffer(int itemId, int price){
+        total+= price * itemsArray[itemId];
+    }
 }
+
