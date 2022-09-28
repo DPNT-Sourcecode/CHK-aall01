a1c51b8840e60c5cfdf02781c00f8399c21c04f7
sbx_user1051 <sbx_user1051@169.254.12.189>
2022-09-28 09:08:57 +0000

    Wed Sep 28 09:08:57 UTC 2022

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -4,8 +4,38 @@ import befaster.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
+        Integer[] productsNumber =  {0,0,0,0};
+        int sum = 0;
+        for (char sku:skus.toCharArray()) {
+            switch (sku){
+                case 'A':
+                    productsNumber[0]++;
+                    break;
+                case 'B':
+                    productsNumber[1]++;
+                    break;
+                case 'C':
+                    productsNumber[2]++;
+                    break;
+                case 'D':
+                    productsNumber[3]++;
+                    break;
+                default:
+                    return -1;
+            }
+        }
+        // adding the prices for the A product
+        sum += (productsNumber[0]/3)*130 +(productsNumber[0]%3)*50;
 
-        return -1;
+        // adding the prices for the B product
+        sum += (productsNumber[1]/2)*45 +(productsNumber[1]%2)*30;
+
+        // C/D product
+        sum += 20 * productsNumber[2];
+        sum += 15 * productsNumber[3];
+
+        return sum;
     }
 }
+
 
