9621d25b958b9da4b8dbf20ccaa52f8f804242e8
sbx_user1051 <sbx_user1051@169.254.12.189>
2022-09-28 10:17:42 +0000

    Wed Sep 28 10:17:42 UTC 2022

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -4,52 +4,18 @@ import befaster.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        int[] productsNumber = new int[26];
-        int sum = 0;
-        for (char sku:skus.toCharArray()) {
-            int ascii = (int) sku;
-            ascii
+        if(checkString(skus)!=true)
+            return -1;
+
+    }
+
+    //function for checking if the input string is valid.
+    public static boolean checkString(String skus){
+        for (char letter:skus.toCharArray()) {
+            if(letter<'A' && letter >'Z')
+                return false;
         }
-        // adding the prices for the A product
-        // and removing the producs from the array
-        if (productsNumber[0]>=5) {
-            sum += (productsNumber[0]/5)*200;
-            int removedProducts = productsNumber[0]/5;
-            productsNumber[0] = productsNumber[0] - removedProducts*5;
-        }
-        if (productsNumber[0]>=3) {
-            sum += (productsNumber[0]/3)*130;
-            int removedProducts = productsNumber[0]/3;
-            productsNumber[0] = productsNumber[0] - removedProducts*3;
-        }
-        sum+= 50 * productsNumber[0];
-
-        // adding the prices for the E product because we need to process them
-        // before the B product as 2Es will remove one B (or at least that's
-        // what i'm understanding from the info provided)
-        if (productsNumber[4]>=2){
-            int pairs_of_e_prod=productsNumber[4] /2;
-            productsNumber[1] =productsNumber[1] - pairs_of_e_prod;
-            if(productsNumber[1]<0)productsNumber[1]=0;
-        }
-        sum+= 40 * productsNumber[4];
-
-        // adding the prices for the B product
-        sum += (productsNumber[1]/2)*45 +(productsNumber[1]%2)*30;
-
-        // C/D product
-        sum += 20 * productsNumber[2];
-        sum += 15 * productsNumber[3];
-
-        // F Product
-        if (productsNumber[5]>=3){
-            int triples_of_e_prod=productsNumber[5] /3;
-            productsNumber[5] = productsNumber[5] - triples_of_e_prod*3;
-            sum+= 20*triples_of_e_prod;
-        }
-        sum+=10*productsNumber[5];
-
-
-        return sum;
+        return true;
     }
 }
+
--- src/main/java/befaster/solutions/CHK/ShoppingCart.java
+++ src/main/java/befaster/solutions/CHK/ShoppingCart.java
@@ -1,2 +1,35 @@
-package befaster.solutions.CHK;public class ShoppingCart {
+package befaster.solutions.CHK;
+
+public class ShoppingCart {
+    int[] itemsArray;
+    int total=0;
+
+    public ShoppingCart(String skus){
+        itemsArray = new int[26];
+        for (char sku:skus.toCharArray()) {
+            int ascii = (int) sku;
+            ascii = ascii-65;
+            itemsArray[ascii]++;
+        }
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void getOneFreeOffer(int sourceItem, int numberOfItemsForDiscount, int discountedItem){
+        if (itemsArray[sourceItem]>=numberOfItemsForDiscount){
+            int pairs_of_prod=itemsArray[sourceItem] /numberOfItemsForDiscount;
+            itemsArray[discountedItem] =itemsArray[discountedItem] - pairs_of_prod;
+            if(itemsArray[discountedItem]<0)itemsArray[discountedItem]=0;
+        }
+    }
+    public void getXForLowerPriceOffer(int itemId, int offerPrice, int noItemsToGetDiscount){
+        if (itemsArray[itemId]>=noItemsToGetDiscount) {
+            total += (itemsArray[itemId]/noItemsToGetDiscount)*offerPrice;
+            int removedProducts = itemsArray[itemId]/noItemsToGetDiscount;
+            itemsArray[itemId] = itemsArray[offerPrice] - removedProducts*noItemsToGetDiscount;
+        }
+    }
 }
+
