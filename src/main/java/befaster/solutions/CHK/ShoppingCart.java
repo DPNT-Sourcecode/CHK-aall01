b8072da02c743e3ae41058edded5e021f7e3781e
sbx_user1051 <sbx_user1051@169.254.12.189>
2022-09-28 10:53:42 +0000

    Wed Sep 28 10:53:42 UTC 2022

--- src/main/java/befaster/solutions/CHK/ShoppingCart.java
+++ src/main/java/befaster/solutions/CHK/ShoppingCart.java
@@ -5,22 +5,39 @@ public class ShoppingCart {
     private int total=0;
 
     public ShoppingCart(String skus){
-        itemsArray = new int[26];
+        // constructor that is using the skus String in order to generate
+        // an arrray containing the number of (each) kind of items
+        // added to the shopping cart
+        // example: for "AAA" - the array will be (3,0,..,0)
+        itemsArray = new int[26];// empty array
+        // iterating through the letters in the skus string
         for (char sku:skus.toCharArray()) {
+            // extracting the ascii code
             int ascii = (int) sku;
+            // subtracting 65 so A will become 0 instead of 65
             ascii = ascii-65;
+            // incrementing the location in the array
             itemsArray[ascii]++;
         }
     }
 
     public int getTotal() {
+        //returns the total price of the shopping cart
+        // nothing fancy
         return total;
     }
 
     public void getOneFreeOffer(int sourceItem, int numberOfItemsForDiscount, int discountedItem){
+        // function used in the scenario where you get one discountedItem free item when you buy
+        // discountedItem*numberOfItemsForDiscount.
+
+        // checking if there are enought items to apply the discount
         if (itemsArray[sourceItem]>=numberOfItemsForDiscount){
+            // calculating the number of "pairs" (i.e. how many times the discount is applied).
             int pairs_of_prod=itemsArray[sourceItem] /numberOfItemsForDiscount;
+            // removing the discountedItem(s) so those will not be paid.
             itemsArray[discountedItem] =itemsArray[discountedItem] - pairs_of_prod;
+            // ensuring that the number of discountedItem(
             if(itemsArray[discountedItem]<0)itemsArray[discountedItem]=0;
         }
     }
@@ -44,4 +61,5 @@ public class ShoppingCart {
         total+= price * itemsArray[itemId];
     }
 }
+
 
